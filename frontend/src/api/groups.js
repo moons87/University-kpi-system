980063786861465a1bfe882799b183a3813af764
moons87 <arlancrosby15@gmail.com>
2026-04-17 20:12:58 +0500

    feat: add updateGroup and getAdvisors to groups API client

--- frontend/src/api/groups.js
+++ frontend/src/api/groups.js
@@ -1,4 +1,6 @@
 import client from './client';
 
-export const getGroups   = ()     => client.get('/groups/').then(r => r.data);
-export const createGroup = (data) => client.post('/groups/', data).then(r => r.data);
+export const getGroups    = ()           => client.get('/groups/').then(r => r.data);
+export const createGroup  = (data)       => client.post('/groups/', data).then(r => r.data);
+export const updateGroup  = (id, data)   => client.patch(`/groups/${id}`, data).then(r => r.data);
+export const getAdvisors  = ()           => client.get('/groups/advisors').then(r => r.data);
